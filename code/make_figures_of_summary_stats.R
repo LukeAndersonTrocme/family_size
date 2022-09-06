36300b4b3c4add690c1524a10798d7a9b58078cb
Luke <luke.anderson.trocme@gmail.com>
2022-09-06 15:06:21 -0400

    adding exploratory figures

--- code/make_figures_of_summary_stats.R
+++ code/make_figures_of_summary_stats.R
@@ -1,24 +1,11 @@
 library(dplyr)
 library(data.table)
 library(ggplot2)
-
-library(plyr) # r2 formula
-library(ggpmisc) # r2 formula on plot
-library(cowplot) # multi panel plot
-
-lm_eqn = function(df){
-  m = lm(y ~ x, df);
-  eq <- substitute(italic(y) == a + b %.% italic(x)*","~~italic(r)^2~"="~r2, 
-                   list(a = format(coef(m)[1], digits = 2), 
-                        b = format(coef(m)[2], digits = 2), 
-                        r2 = format(summary(m)$r.squared, digits = 3)))
-  as.character(as.expression(eq));                 
-}
+library(ggpubr) # r2 formula on plot
 
 make_summary_stat_plots <- function(name = "Name", 
                                     filename = "/Users/luke/Documents/family_size/data/Saguenay_ind_relative_family_size.csv",
                                     output_dir = "/Users/luke/Documents/family_size/figures/"){
-  
   infile <- 
     fread(filename) %>%
     filter(!is.na(decade)) %>%
@@ -29,29 +16,25 @@ make_summary_stat_plots <- function(name = "Name",
   infile$group <- paste0(infile$great,".",infile$grand,".","parents_",infile$time_bin)
   
   # Part 1
-  
-    town_corr <-
+  town_corr <-
     infile %>%
     group_by(t, great, grand, time_bin) %>%
     dplyr::summarise(correl = cor(focal_family_size, relative_family_size),
                      count = n()) %>%
-    ggplot(., aes(x = as.factor(time_bin), y = correl, color = paste0(great,".",grand,".","parent"))) +
-    geom_boxplot(outlier.shape = NA) +
-    geom_point(size = 0.01, position = position_jitterdodge()) +
+    ggplot(., aes(x = as.factor(time_bin), y = correl)) +
+    geom_hline(yintercept = 0, size = 0.5) +
+    geom_jitter(size = 0.01, color = "gray60") +
+    geom_boxplot(outlier.shape = NA, fill = NA) +
     guides(color=guide_legend(title="Relative")) +
     ggtitle(name) +
     labs(x="Time") +
     theme_bw() +
-    theme(plot.title = element_text(hjust = 0.5))
+    theme(plot.title = element_text(hjust = 0.5)) +
+    facet_grid(.~ paste0(great,".",grand,".","parent"))
   
   ggsave(town_corr, filename = paste0(output_dir,name,"_corr_fs.jpg"), height = 4, width = 7)
   
   # Part 2
-  
-  eq <- ddply(infile %>% dplyr::rename(x = focal_family_size, y = relative_family_size),.(group),lm_eqn)
-  
-  label_df <- inner_join(eq, infile) %>% group_by(group, V1) %>% dplyr::summarise(lab_x = 15, lab_y = 29)
-  
   op <-
     ggplot(infile, 
            aes(x = focal_family_size, y = relative_family_size)) +
@@ -60,12 +43,11 @@ make_summary_stat_plots <- function(name = "Name",
     scale_x_continuous(limits = c(0,30))+
     scale_y_continuous(limits = c(0,30))+
     theme_bw() +
-    facet_wrap(.~group, scales = "free_y", labeller = as_labeller(eq, 
-                                                                  default = label_parsed)) +
-    geom_text(data = label_df, aes(x = lab_x, y = lab_y, label=V1), parse = TRUE, inherit.aes=FALSE) +
+    facet_grid(as.factor(time_bin)~paste0(great,".",grand,".","parent")) +
+    stat_cor(aes(label = ..rr.label..), # paste(..rr.label.., ..p.label.., sep = "~`,`~")
+             label.x = 15, r.accuracy = 0.001) +
     ggtitle(name) +
     theme(plot.title = element_text(hjust = 0.5))
-  
-  
-  ggsave(op, filename = paste0(output_dir,name,"_ind_relative_family_size.jpg"), height = 8, width = 12)
+
+  ggsave(op, filename = paste0(output_dir,name,"_ind_relative_family_size.jpg"), height = 8, width = 6)
 }
